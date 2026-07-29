7d517095ef032519a1b5ee618017e53343a308c9
qiqi.zhou <qiqi.zhou@mihoyo.com>
2026-07-29 13:07:40 +0800

    chore: init shadcn ui for the site

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,22 @@
 import "@testing-library/jest-dom/vitest"
+
+// jsdom stubs required by cmdk and Radix primitives
+if (!Element.prototype.scrollIntoView) {
+  Element.prototype.scrollIntoView = () => {}
+}
+if (!Element.prototype.hasPointerCapture) {
+  Element.prototype.hasPointerCapture = () => false
+}
+if (!Element.prototype.setPointerCapture) {
+  Element.prototype.setPointerCapture = () => {}
+}
+if (!Element.prototype.releasePointerCapture) {
+  Element.prototype.releasePointerCapture = () => {}
+}
+if (typeof globalThis.ResizeObserver === "undefined") {
+  globalThis.ResizeObserver = class {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  } as unknown as typeof ResizeObserver
+}
